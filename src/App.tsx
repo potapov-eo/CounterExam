baf6aff6db20a948321bbe1dea3978d4ea3b7f33
Evgeniy Potapov <potapov.eo@mail.ru>
2020-10-10 22:23:38 +0300

    4 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ButtonComponent} from "./ButtonComponent";
+import {Display} from "./Display";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const maxCount = 7
+    const minCount = 4
+    let [count, setCount] = useState<number>(minCount)
+
+
+    let changeCount = () => {
+        if (count < maxCount) {
+            setCount(count + 1)
+        }
+    }
+    let resetCount = () => setCount(minCount)
+
+    return (
+        <div className="App">
+
+            <Display
+                count={count} maxCount={maxCount}/>
+            <div className={"displayButton"}>
+                <ButtonComponent
+                    id={1}
+                    count={count}
+                    title={"INT"}
+                    changeCount={changeCount}
+                    maxCount={maxCount}
+                    minCount={minCount}
+
+                />
+                <ButtonComponent
+                    id={2}
+                    count={count}
+                    title={"RESET"}
+                    changeCount={resetCount}
+                    maxCount={maxCount}
+                    minCount={minCount}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Display.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type DisplayType = {
+    count: number
+    maxCount:number
+}
+
+export function Display(props: DisplayType) {
+    const error = props.count === props.maxCount ? "error" : "display"
+
+    return <div className={error}>{props.count}</div>
+
+}
+
