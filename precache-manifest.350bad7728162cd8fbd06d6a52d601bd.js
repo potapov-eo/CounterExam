a1cfbe355dcb06eb2b87ce3894a23ed3f4417cd8
potapov-eo <potapov.eo@yandex.ru>
2020-11-07 23:42:56 +0300

    Updates

---
+++ precache-manifest.350bad7728162cd8fbd06d6a52d601bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a619122b40fe9a464c3c93219ab46af",
+    "url": "/CounterExam/index.html"
+  },
+  {
+    "revision": "25c7170f344010a2184a",
+    "url": "/CounterExam/static/css/main.63fdab85.chunk.css"
+  },
+  {
+    "revision": "05f5479f4b3e1ec67838",
+    "url": "/CounterExam/static/js/2.d99cf23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CounterExam/static/js/2.d99cf23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c7170f344010a2184a",
+    "url": "/CounterExam/static/js/main.4d20b50f.chunk.js"
+  },
+  {
+    "revision": "b929a8dec947b8b4ab92",
+    "url": "/CounterExam/static/js/runtime-main.d08de18c.js"
+  }
+]);
